57f5789734016493e18405b5ff6a109fdcfb5773
Natalie <tnatkinson95@gmail.com>
2025-08-11 07:27:49 +1000

    refactor: rename AGL Energy to Penrith Bill Collection

--- custom_components/penrith_bill_collection/penrith_bin_collection.py
+++ custom_components/penrith_bill_collection/penrith_bin_collection.py
@@ -1,37 +1,52 @@
-import httpx
 import functools
 import json
+from collections import defaultdict
+
+import httpx
+
+from .const import WEBSITE_URL
+
 
 def get_endpoint(website, endpoint):
     r = httpx.get(f"{website}{endpoint}")
-    return(r.text)
+    return r.text
+
 
 get_url = functools.partial(get_endpoint, WEBSITE_URL)
+
 
 def get_localities():
     return json.loads(get_url("localities.json"))["localities"]
 
+
 def find_locality(localities, locality_name):
     return [locality for locality in localities if locality["name"] == locality_name][0]
+
 
 def get_street(locality_id):
     return json.loads(get_url(f"streets.json?locality={locality_id}"))["streets"]
 
+
 def find_street(streets, street_name):
     return [street for street in streets if street["name"] == street_name][0]
+
 
 def get_properties(street_id):
     return json.loads(get_url(f"properties.json?street={street_id}"))["properties"]
 
+
 def find_property(properties, property_name):
     return [property for property in properties if property["name"] == property_name][0]
 
-def get_calendar(property_id, start="2024-12-31T13:00:00.000Z", end="2025-12-30T13:00:00.000Z"):
-    dates = json.loads(get_url(f"properties/{property_id}.json?start={start}&end={end}"))
+
+def get_calendar(
+    property_id, start="2024-12-31T13:00:00.000Z", end="2025-12-30T13:00:00.000Z"
+):
+    dates = json.loads(
+        get_url(f"properties/{property_id}.json?start={start}&end={end}")
+    )
     grouped_by_event = defaultdict(list)
     for event in dates:
         key = event["event_type"]
         grouped_by_event[key].append(event)
     return grouped_by_event
-
-
--- custom_components/penrith_bill_collection/sensor.py
+++ custom_components/penrith_bill_collection/sensor.py
@@ -1,20 +1,17 @@
-"""AGL Energy sensor platform."""
+"""Penrith Bill Collection sensor platform."""
+# ruff: noqa
+
 import logging
 import re
 from datetime import timedelta
 from typing import Any, Callable, Dict, Optional
-from urllib import parse
 
 
 import voluptuous as vol
 
 from homeassistant.components.sensor import PLATFORM_SCHEMA
 from homeassistant.const import (
-    ATTR_NAME,
     CONF_ACCESS_TOKEN,
-    CONF_NAME,
-    CONF_PATH,
-    CONF_URL,
 )
 from homeassistant.helpers.aiohttp_client import async_get_clientsession
 import homeassistant.helpers.config_validation as cv
@@ -26,14 +23,20 @@ from homeassistant.helpers.typing import (
 )
 
 from .const import (
-    WEBSITE_URL,
     ATTR_HOME_LOCALITY,
     ATTR_HOME_STREET,
     ATTR_HOME_PROPERTY,
-    ATTR_NEXT_WASTE_COLLECTION
+    ATTR_NEXT_WASTE_COLLECTION,
 )
 
-from .penrith_bin_collection import get_localities, get_street, get_properties, get_calender, find_locality, find_street, find_property
+from .penrith_bin_collection import (
+    get_localities,
+    get_street,
+    get_properties,
+    find_locality,
+    find_street,
+    find_property,
+)
 
 _LOGGER = logging.getLogger(__name__)
 # Time between updating data from GitHub
@@ -41,15 +44,12 @@ SCAN_INTERVAL = timedelta(minutes=10)
 
 CONF_REPOS = "repositories"
 
-PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
-    {
-        vol.Required(CONF_ACCESS_TOKEN): cv.string
-    }
-)
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({vol.Required(CONF_ACCESS_TOKEN): cv.string})
 
 LINK_RE = re.compile(
     r"\<(?P<uri>[^>]+)\>;\s*" r'(?P<param_type>\w+)="(?P<param_value>\w+)"(,\s*)?'
 )
+
 
 async def async_setup_entry(
     hass: core.HomeAssistant,
@@ -110,21 +110,21 @@ class NextWasteCollectionSensor(Entity):
 
     async def async_update(self):
         localities = get_localities()
-        
+
         selected_locality = find_locality(localities, ATTR_HOME_LOCALITY)
         selected_locality_id = selected_locality["id"]
-        
+
         streets = get_street(selected_locality_id)
-        
+
         selected_street = find_street(streets, ATTR_HOME_STREET)
         selected_street_id = selected_street["id"]
-        
+
         properties = get_properties(selected_street_id)
         selected_property = find_property(properties, ATTR_HOME_PROPERTY)
         selected_property_id = selected_property["id"]
-        
+
         calendar = get_calendar(selected_property_id)
-        
+
         for event in calendar[EVENT_TYPE.Waste.value]:
             self.attrs[ATTR_NEXT_WASTE_COLLECTION] = event["dow"][0]
 
